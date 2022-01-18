b33c479cdb52393dc312e9e9848f9cd0835232f8
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-01-19 00:12:51 +0300

    added some logic to the component: Notes

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Button, Card } from "react-bootstrap";
 import { Note } from "../models/note.model";
 
 interface INotesProps {
@@ -6,7 +7,20 @@ interface INotesProps {
 }
 
 const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
-  return <div>Note Card</div>;
+  return (
+    <div className="mb-3">
+      <Card style={{ backgroundColor: note.color }}>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+          <Button className="mt-3" variant="danger">
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Notes;
